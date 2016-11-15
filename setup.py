3f29c4bebbc7d70891c29a9249b9f731de5da363
thin <thomas@i-n.dk>
2016-11-15 17:10:53 +0100

    CHANGED: Bumped the version number to V0.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     setup(
         zip_safe=False,
         name="soilfreeze1d",
-        version="0.0",
+        version="0.4",
         description="Package for computing ground temperatures using finite difference method.",
         author="Thomas Ingeman-Nielsen",
         author_email="tin@byg.dtu.dk",
